e71672f71e78c0edbc36da09e85367926cf72cc7
whispeed <whispeed@163.com>
2017-03-22 22:03:48 +0800

    create movies index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "您已进入电影影评网站！"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+   resources :movies
+   root 'welcome#index'
 end
