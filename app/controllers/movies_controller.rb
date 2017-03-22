4f2e0bc176ef916e94e49cbe97783bda58e64145
whispeed <whispeed@163.com>
2017-03-22 22:21:17 +0800

    implement movies#update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,14 @@ class MoviesController < ApplicationController
       redirect_to movies_path
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+
+    @movie.update(movie_params)
+
+    redirect_to movies_path, notice: "更新成功"
+  end
+
   private
 
   def movie_params
