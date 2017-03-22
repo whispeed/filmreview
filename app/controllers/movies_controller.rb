2ca47a914594729d6634d6e66a00a90a25defe04
whispeed <whispeed@163.com>
2017-03-22 23:23:33 +0800

    use before_action to find_movie_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @movies = Movie.all
   end
@@ -9,11 +10,6 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "你无权这么做！不要耍小聪明！"
-    end
   end
 
   def new
@@ -32,12 +28,6 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "你无权这么做！不要耍小聪明！"
-    end
-
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "更新成功"
     else
@@ -46,17 +36,18 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "你无权这么做！不要耍小聪明！"
-    end
-
     @movie.destroy
     redirect_to movies_path, alert: "已删除影评"
   end
 
   private
+
+  def find_movie_and_check_permission
+    @movie = Movie.find(params[:id])
+    if current_user != @movie.user
+      redirect_to root_path, alert: "你无权这么做！不要耍小聪明！"
+    end
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :description)
